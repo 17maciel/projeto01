beb50a8557ede45df4a3d78c71daf17e85539649
17maciel <leandromaciel.br@gmail.com>
2024-11-12 21:10:54 -0300

    parte 01

---
+++ app.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_page():
+    url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-256-gb-titnio-deserto-distribuidor-autorizado/p/MLB1040287840#polycard_client=search-nordic&wid=MLB3846027829&sid=search&searchVariation=MLB1040287840&position=3&search_layout=stack&type=product&tracking_id=c53122b0-4d92-48b3-93de-e1afccc1f2e4"
+    response = requests.get(url)
+    return response.text
+
+def parse_page(html):
+    soup = BeautifulSoup(html, "html.parser")
+    product_name = soup.find("h1", {"class": "ui-pdp-title"}).get_text()
+    product_price = soup.find_all("span", {"class": "andes-money-amount__fraction"})
+    old_price: int = int(product_price[0].get_text().replace('.', ''))
+    new_price: int = int(product_price[1].get_text().replace('.', ''))
+    installment_price : int = int(product_price[2].get_text().replace('.', ''))
+
+    return {
+        "product_name": product_name,
+        "old_price": old_price,
+        "new_price": new_price,
+        "installment_price": installment_price
+    }
+
+if __name__ == "__main__":
+    page_content = fetch_page()
+    product_info = parse_page(page_content)
+    print(product_info)
+
